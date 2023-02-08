50aa2a88799a83e906893f4d31c653a8c6f6e217
Fernanda <sfernanda1@outlook.com>
2023-02-08 15:15:15 -0300

    Implementando conteúdo do módulo 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { TemplateRef, ViewChild } from '@angular/core';
+import { ModalService, ModalRef } from './shared/components/modal/services/modal.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'a11y-p2';
+
+  //Injetando templateRef
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
+  public modalRef: ModalRef;
+
+  constructor(private modalService: ModalService){}
+
+  //chamada para exibir o modal
+  public show(): void {
+   this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Datail'
+    })
+
+  }
+
 }
+
