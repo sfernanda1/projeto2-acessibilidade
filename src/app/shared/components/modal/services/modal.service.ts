695286edcf215d5214437dcb3b9e8a07e438e35e
Fernanda <sfernanda1@outlook.com>
2023-02-16 15:31:06 -0300

    adicionando conteúdo módulo 2

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,21 +1,32 @@
-import { Injectable, TemplateRef } from "@angular/core";
+import { BodyInjectorService } from './../../../services/body-injector';
+import { ModalComponent } from './../modal.component';
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector, TemplateRef } from "@angular/core";
+import { ModalConfig } from "../interfaces/modal-config";
 
 @Injectable()
 export class ModalService{
-
-  public open(config: ModalConfig) : ModalRef{
-    return new ModalRef();
+  private componentFactory : ComponentFactory<ModalComponent>
+  constructor(componentFactoryResolver: ComponentFactoryResolver, private injector: Injector, private bodyInjector: BodyInjectorService){
+    this.componentFactory = componentFactoryResolver.resolveComponentFactory(ModalComponent);
   }
 
-}
+  public open(config: ModalConfig) : ModalRef{
+    const componentRef = this.createComponentRef();
+    componentRef.instance.config = config;
+    this.bodyInjector.stackeBeforeAppRoot(componentRef);
+    return new ModalRef(componentRef);
+  }
 
-export interface ModalConfig{
-  templateRef: TemplateRef<any>;
-  title: string;
+  private createComponentRef(): ComponentRef<ModalComponent>{
+    return this.componentFactory.create(this.injector);
+  }
 }
 
 export class ModalRef{
+  constructor(private componenRef: ComponentRef<ModalComponent>){}
   public close(): void{
-
+    this.componenRef.destroy();
   }
 }
+
+//O ComponentRef é um artefato com Angular que encapsula uma instância do componente criado dinamicamente. Já ModalRef é uma criação nossa que encapsula um ComponentRef possuindo apenas o método close sem export a referência para o desenvolvedor cliente da nossa API.
